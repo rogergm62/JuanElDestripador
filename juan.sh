a217ac1bbcad70a747a56ef4541bb821f7649b3e
rogergm62 <97410186+rogergm62@users.noreply.github.com>
2026-02-07 14:32:18 -0800

    Update juan.sh

--- juan.sh
+++ juan.sh
@@ -1,9 +1,16 @@
+# CIS 450 Pen Testing. Rogelio G.
+#This script automates the use of John the Ripper
+# It finds and tests several wordlists located in "seclist."
+
+# The following line is where the hashes are located
 HASF="/home/kali/.msf4/loot/20260206155050_tyrell_192.168.1.142_windows.hashes_06>
- 2 BASE="/usr/share/seclists/Passwords/Common-Credentials"
- 3 for WORDLIST in $(find "$BASE" -type f -name "*.txt"); do
- 4    echo "[*] Trying wordlist: $WORDLIST"
- 5    john --format=NT --rules=Jumbo --wordlist="$WORDLIST" "$HASF"
- 6 done
- 7
- 8 echo "Passwords"
- 9 john --show --format=NT "$HASF"
+# This is where the world lists are located, I recoment start with Common Credentials
+# Using "seclists/Passwords" or just "/seclist" could be time-consuming.
+BASE="/usr/share/seclists/Passwords/Common-Credentials"
+for WORDLIST in $(find "$BASE" -type f -name "*.txt"); do
+    echo "[*] Trying wordlist: $WORDLIST"
+    john --format=NT --rules=Jumbo --wordlist="$WORDLIST" "$HASF"
+done
+#
+echo "Passwords"
+john --show --format=NT "$HASF"
