54cc2ba00646a4235b7c9b227720c84d0a6a3f4b
rogergm62 <97410186+rogergm62@users.noreply.github.com>
2026-02-07 17:39:36 -0800

    Update juan.sh

--- juan.sh
+++ juan.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 # CIS 450 Pen Testing. Rogelio G.
-#This script automates the use of John the Ripper
+#This script automates the use of John the Ripper and Hashcat
 # It finds and tests several wordlists located in "seclist."
 
+# ************ JOHN THE RIPPER *****************************
 # If you want to delete the stored passwords uncommnet the following line:
 rm ~/.john/john.pot
 
@@ -27,3 +28,17 @@ done
 #
 echo "Passwords"
 john --show --format=NT "$HASF"
+
+# ***************** HASHCAT *******************************
+# This loop tests one wordlist after another:
+for WORDLIST in $(find "$BASE" -type f -name "*.txt"); do
+    echo "****** TRYING WORLIST: $WORDLIST"
+    
+    # Modify the format according to the type of hashes, e.g., 1000 for NT
+    # I recommend adding a wordlist plus a rule like best66 or rockyou-30000 to compare to Korelogic or Jumbo.
+    # Check ls /usr/share/hashcat/rules/ for a complete list of rules
+    time hashcat -m 1000 -a 0 "$HASF" "$WORDLIST" -r /usr/share/hashcat/rules/best66.rule
+done
+#
+echo "Passwords"
+hashcat -m 1000 --show "$HASF"
